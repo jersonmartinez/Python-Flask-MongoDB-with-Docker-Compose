03039162966cfed7578bcd17013d7e827e91aad7
Antonio Moreno <jersonmartinezsm@gmail.com>
2022-07-26 20:02:06 -0600

    Making the connection to the database management system MongoDB

--- app/app.py
+++ app/app.py
@@ -1,9 +1,6 @@
 import json
-from pydoc import resolve
-from urllib import response
-from flask import Flask, jsonify, request, json, Response
+from flask import Flask, json, Response
 from pymongo import MongoClient
-import logging as log
 from pymongo.errors import ServerSelectionTimeoutError, OperationFailure
 
 app = Flask(__name__)
@@ -11,21 +8,17 @@ app = Flask(__name__)
 class ConnectionMongoDB:
     def __init__(self, data):
         self.server     = data['server']
+        self.port       = data['port']
         self.username   = data['username']
         self.password   = data['password']
         self.db         = data['db']
         
     def getDB(self):
-
-        # client = MongoClient("cs_mongodb", 27017, serverSelectionTimeoutMS=500)
-        
-        # db = client.db_name
-
-        mongoClient = MongoClient("mongodb://root-crashell:password-crashell@cs_mongodb:27017/?authMechanism=DEFAULT&authSource=db_crashell", serverSelectionTimeoutMS=500)
+        mongoClient = MongoClient("mongodb://" + str(self.username) + ":" + str(self.password) + "@" + str(self.server) + ":" + str(self.port) + "/?authMechanism=DEFAULT&authSource=" + str(self.db), serverSelectionTimeoutMS=500)
 
         try:
             if mongoClient.admin.command('ismaster')['ismaster']:
-                return "Connected!"
+                return "Connected to the MongoDB Server!"
         except OperationFailure:
             return ("Database not found.")
         except ServerSelectionTimeoutError:
@@ -33,24 +26,20 @@ class ConnectionMongoDB:
 
 @app.route('/api')
 def get_api():
-    # return ("Nada de nada")
     data = {
         "server":   "cs_mongodb",
+        "port":     '27017',
         "username": "root-crashell",
         "password": "password-crashell",
-        "db":       "db-crashell"
+        "db":       "db_crashell"
     }
 
-    response = str(ConnectionMongoDB(data).getDB())
-    # print(response)
-    # response = {'message': 'List all teams available in the DB.'}
-    # return jsonify(response)
-    return response
-    # return Response(response=json.dumps(response), status=200, mimetype='application/json')
+    response = ConnectionMongoDB(data).getDB()
+    return Response(response=json.dumps(response), status=200, mimetype='application/json')
 
 @app.route('/', methods=('GET', 'POST'))
 def hello():
-    return '¡Hola, Crashell!'
+    return '<h1 style="background-color: blue; color: white; padding: 20px; text-align:center;">Hello, Crashell!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
